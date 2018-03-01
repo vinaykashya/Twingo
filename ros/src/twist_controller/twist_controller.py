55631c0ef98755016cebfae4f7c6e08cd797eb78
Tom Zheng <sotomso@hotmail.com>
2018-03-01 06:53:08 -0800

    added easy throttle control

--- ros/src/twist_controller/twist_controller.py
+++ ros/src/twist_controller/twist_controller.py
@@ -16,11 +16,22 @@ class Controller(object):
         max_lat_accel = kwargs['max_lat_accel']
         max_steer_angle = kwargs['max_steer_angle']
 
+        self.linear_pid = PID(kp=0.1, ki=0.0, kd=0.03, mn=kwargs['decel_limit'], mx=kwargs['accel_limit'])
         self.yaw_controller = YawController(wheel_base, steer_ratio, min_speed, max_lat_accel, max_steer_angle)
+        self.last_time = rospy.get_time()
         pass
+
+    def get_interval(self):
+    	now = rospy.get_time()
+    	interval = now - self.last_time
+    	self.last_time = now
+    	return interval
 
     def control(self, linear_velocity, angular_velocity, current_velocity):
         # TODO: Change the arg, kwarg list to suit your needs
         # Return throttle, brake, steer
+        interval = self.get_interval()
+        linear_err = linear_velocity - current_velocity
+       	throttle = self.linear_pid.step(linear_err, interval)
         steer = self.yaw_controller.get_steering(linear_velocity, angular_velocity, current_velocity)
-        return 0.3, 0., steer
+        return throttle, 0., steer
